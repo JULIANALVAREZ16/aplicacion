7d4834abba352071e543b0a4a54132befb5a229c
Estudiantes <julianalvarez199423@gmail.com>
2023-05-02 16:29:47 -0500

    update

--- index.php
+++ index.php
@@ -9,19 +9,20 @@
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-    <form>
+    <form method="post" action="registrar.php">
     <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Identificador</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="text" class="form-control" id="Identificador" name="Identificador" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Nombre</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="text" class="form-control" id="Nombre" name="Nombre" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Detalle</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="text" class="form-control" id="Detalle" name="Detalle" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   
   <button type="submit" class="btn btn-primary">Registarr</button>
---
+++ registrar.php
@@ -0,0 +1,21 @@
+<?php
+    include 'conexion.php';
+
+    $id = $_POST["Identificador"];
+    $nombre = $_POST["Nombre"];
+    $detalle = $_POST["Detalle"];
+
+    
+    $sql="INSERT INTO categorias (Identificador, Nombre, Detalle)
+          values('$id', '$nombre', '$detalle')";
+
+
+    $res = 0;
+    if($conexion->query($sql) === TRUE) {
+      echo('Aceptado');
+    }
+    else{
+      echo('Rechazado');
+    }
+    mysqli_close($conexion);  // Cierra la conexion
+
